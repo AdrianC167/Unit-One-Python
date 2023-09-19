99d8598b99ea48674b469aa256175100c238ed65
AdrianC167 <ac232126110@gmail.com>
2023-09-19 09:50:37 -0400

    exercises

---
+++ benchmark.py
@@ -0,0 +1,45 @@
+# Exercise 1: Display your name in the console
+
+print("Adrian")
+
+# Exercise 2: Create a variable called favoriteShow and set it equal to my favorite show
+
+favoriteShow = "lucifer"
+
+# Exercise 3: Create two variables called heightg and age and set it equal to my height and age then display in console the product of them. 
+
+height = 5.65
+age = 17
+product = height * age
+print(product)
+
+# Exercise 4: Display "im going to break" in the console 
+
+print("im going to break")
+
+# Exercise 5: create a list of strings called "fav_food" and put 3 of my favorite food items inside the list
+
+fav_food = ["popeyes","mcdonalds","wendys"]
+
+# Exercise 6: Create a conditional statement that cheks if your age is greater than or equal to 21. if trye then display "you can rent a car" otherwise display your beat
+
+if age >= 21:
+    print("you can rent a car")
+else:
+    print("your beat")
+
+# Exercise 7: ask the user for their name then display in the console hello their name
+
+print("What is your name?") 
+name = input()
+print("Hello " + name)
+
+# Exercise 8: create a loop that will display every item from fav-food on a seperate line in the console.
+
+for list in fav_food:
+    print(list)
+
+# Exercise 9: create a while loop that will never run
+
+while age == 16:
+    print("hi")
